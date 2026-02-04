83d59a038bed996ee71c5b7938aad15ae50ebf02
itshoanggg <hoangdanghoctiengnhat@gmail.com>
2026-02-04 21:55:28 +0700

    add dtos

---
+++ EWMS/DTOs/InventoryCheckDto.cs
@@ -0,0 +1,14 @@
+﻿namespace EWMS.DTOs
+{
+    public class InventoryCheckDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public int RequestedQuantity { get; set; }
+        public int CurrentStock { get; set; }
+        public int ExpectedIncoming { get; set; }
+        public int PendingOutgoing { get; set; }
+        public int AvailableStock { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ EWMS/DTOs/InventoryCheckRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace EWMS.DTOs
+{
+    public class InventoryCheckRequest
+    {
+        public int WarehouseId { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+        public List<ProductQuantityDto> Products { get; set; } = new();
+    }
+}
