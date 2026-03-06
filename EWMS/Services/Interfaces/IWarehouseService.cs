3bf145d758f641d1c9d1c213ded6de3596679b53
itshoanggg <hoangdanghoctiengnhat@gmail.com>
2026-03-06 23:13:38 +0700

    Add warehouse service

---
+++ EWMS/Services/Interfaces/IWarehouseService.cs
@@ -0,0 +1,21 @@
+using EWMS.ViewModels;
+
+namespace EWMS.Services.Interfaces;
+
+public interface IWarehouseService
+{
+    Task<WarehouseListViewModel> GetWarehousesAsync(string? searchQuery, int page, int pageSize);
+    Task<WarehouseDetailsViewModel?> GetWarehouseDetailsAsync(int warehouseId, int page = 1, int pageSize = 10);
+    Task<int> CreateWarehouseAsync(CreateWarehouseViewModel model);
+    Task<bool> UpdateWarehouseAsync(EditWarehouseViewModel model);
+    Task<bool> DeleteWarehouseAsync(int warehouseId);
+    Task<EditWarehouseViewModel?> GetWarehouseForEditAsync(int warehouseId);
+    
+    Task<LocationListViewModel> GetLocationsAsync(string? searchQuery, int? warehouseId, int page, int pageSize);
+    Task<LocationDetailsViewModel?> GetLocationDetailsAsync(int locationId);
+    Task<int> CreateLocationAsync(CreateLocationViewModel model);
+    Task<bool> UpdateLocationAsync(EditLocationViewModel model);
+    Task<bool> DeleteLocationAsync(int locationId);
+    Task<EditLocationViewModel?> GetLocationForEditAsync(int locationId);
+    Task<CreateLocationViewModel> PrepareCreateLocationViewModelAsync(int? preselectedWarehouseId = null);
+}
