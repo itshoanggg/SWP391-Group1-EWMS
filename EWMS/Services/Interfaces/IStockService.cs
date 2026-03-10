39233102910480e6f4fa5ca52bb9a149fbc507a5
TuanTNHE171132 <121917907+tuandeeptry2k2@users.noreply.github.com>
2026-03-10 10:40:46 +0700

    update internal transfer

--- EWMS/Services/Interfaces/IStockService.cs
+++ EWMS/Services/Interfaces/IStockService.cs
@@ -8,5 +8,6 @@ namespace EWMS.Services.Interfaces
         Task<IEnumerable<LocationDTO>> GetLocationsByRackAsync(int warehouseId, string rack);
         Task<IEnumerable<ProductInLocationDTO>> GetProductsByLocationAsync(int locationId);
         Task<StockSummaryDTO> GetStockSummaryAsync(int warehouseId);
+        Task<bool> PerformInternalTransferAsync(int warehouseId, int fromLocationId, int toLocationId, int productId, int quantity, int userId, string? reason);
     }
 }
