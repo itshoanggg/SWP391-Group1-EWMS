28ceb311e1395dd14141066992f3c1aa67818757
itshoanggg <hoangdanghoctiengnhat@gmail.com>
2026-03-14 21:21:36 +0700

    DTOs message

---
+++ EWMS/DTOs/SalesOrderResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EWMS.DTOs
+{
+    public class SalesOrderResultDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public int? SalesOrderId { get; set; }
+        public int UserId { get; set; }
+    }
+}
